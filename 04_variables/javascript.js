2f522ec4bd801262cbdb82e5f250a762a2d95431
mehdijafarzadeh <jafarzade.me@gmail.com>
2024-08-02 15:16:57 +0200

    add datatypes

---
+++ 04_variables/javascript.js
@@ -0,0 +1,12 @@
+let a = 5;
+console.log(a);
+
+let b = 10;
+console.log(b);
+
+let z = a + b;
+console.log(z);
+
+let nav = 'kani';
+console.log(nav);
+
---
+++ 05_datatypes/javascript.js
@@ -0,0 +1,58 @@
+let hejmar = 5.1;    // number
+
+console.log(hejmar * 2);
+
+console.log(hejmar + 2);
+console.log(hejmar - 4);
+console.log(hejmar / 2);
+
+console.log(typeof hejmar);
+
+let nav = 'kani'; // string
+let pashnav = 'mani';
+
+console.log(nav + ' '+ pashnav);
+console.log(typeof nav);
+
+let bool = false; // boolean
+console.log(typeof bool);
+let num = 6 > 5;
+console.log(num);
+
+let user = null; // null
+
+let pass;
+console.log(pass); // undefined
+
+
+
+
+
+
+
+
+// let a = 5;
+// console.log(a);
+
+// let b = 10;
+// console.log(b);
+
+// let z = a + b;
+// console.log(z);
+
+// let nav = 'kani';
+// console.log(nav);
+
+// let c = 10;
+// console.log(c);
+
+// c = 20;
+// console.log(c);
+
+// const d = 40;
+// console.log(d);
+
+// d = 50;
+// em nikarin variablen 'const' biguheriinin.
+
+
